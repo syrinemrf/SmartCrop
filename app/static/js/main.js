e7da8c0930d88152aed043da77fe281738991dbf
syrinemrf <syrine.mrf@gmail.com>
2025-12-04 00:19:18 +0100

    UI improvements: remove Recent Activity from profile, add predict translations, make flash messages permanent

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -13,15 +13,6 @@ const CropApp = {
 
     // Event Listeners
     setupEventListeners: function() {
-        // Auto-dismiss alerts after 5 seconds
-        const alerts = document.querySelectorAll('.alert');
-        alerts.forEach(alert => {
-            setTimeout(() => {
-                const bsAlert = new bootstrap.Alert(alert);
-                bsAlert.close();
-            }, 5000);
-        });
-
         // Form validation
         const forms = document.querySelectorAll('.needs-validation');
         forms.forEach(form => {
